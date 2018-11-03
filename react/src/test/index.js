25e20b615ddb3eccfc1a69ac8e41f154bb75a59b
ymtsk0131 <ymtsk0131@live.jp>
2018-11-03 22:09:57 +0900

    delete test controller

--- react/src/test/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-console.log("Hello from test/index.js")
-ReactDOM.render(
-    <h1 className="hello-react">Hello from React!</h1>,
-    document.getElementById('content')
-    );
